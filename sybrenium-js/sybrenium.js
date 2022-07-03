8cec7231cb4a291b263edd2b1133f9b6a60eaa53
Yaseen <fantasticyaseenshariff@gmail.com>
2022-07-03 13:34:14 +0530

    scroll-to-the-top added

--- sybrenium-js/sybrenium.js
+++ sybrenium-js/sybrenium.js
@@ -1,33 +1,29 @@
-// var theNewScript = document.createElement("script");
-// theNewScript.type = "text/javascript";
-// theNewScript.src = "https://code.jquery.com/jquery-3.6.0.min.js";
-// document.getElementsByTagName("head")[0].appendChild(theNewScript);
 function include(file) {
-
   var script  = document.createElement('script');
   script.src  = file;
   script.type = 'text/javascript';
   script.defer = true;
-
   document.getElementsByTagName('head').item(0).appendChild(script);
 }
 include("https://code.jquery.com/jquery-3.6.0.min.js");
 include("https://cdn.jsdelivr.net/npm/pyscript-js@1.2.4/dist/js/pyscript.min.js");
 var waitForjQLoad = function(){if(typeof jQuery != "undefined"){sybreniumFunctions();}else{window.setTimeout(waitForjQLoad, 1);}};window.setTimeout(waitForjQLoad, 1);
-// print("hi")
 
 function sybreniumFunctions(){
+  // sample test code
+  $(".sampleTest").text("Sample Test") // A sample text to test it works
+
+  // back to the top (scroll to the top function)
   $(window).scroll(function() {
       if ($(this).scrollTop()) {
-          $('.scroll-to-top').fadeIn();
+          $('.scroll-to-the-top').fadeIn();
       } else {
-          $('.scroll-to-top').fadeOut();
+          $('.scroll-to-the-top').fadeOut();
       }
   });
 
-  $(".scroll-to-top").click(function() {
+  $(".scroll-to-the-top").click(function() {
       $("html, body").animate({scrollTop: 0}, 1000);
    });
 
-   $(".sampleTest").text("Sample Test")
 }
